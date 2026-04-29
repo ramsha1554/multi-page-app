193f110aae845fcdb145139a1951b4b53b7f862d
ramsha1554 <work.rm.13.dec@gmail.com>
2026-04-29 12:00:00 +0530

    cleaned up some messy code, added comments

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// entrypoint for the react app
+// we render the main App component inside the root div of index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
